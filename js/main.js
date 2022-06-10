4e0a5dada83b24d54b3f5459ebbdf201b05cc805
Gustaw Daczkowski <01149695@pw.edu.pl>
2022-06-10 23:32:40 +0200

    clean up code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+"use strict";
+requestAnimationFrame(plot);
+
 var d1 = [];
 var d11;
 var d2 = [];
@@ -5,22 +8,23 @@ var d3 = [];
 var d4 = [];
 var d5 = [];
 var d7;
-var pi = 3.14159265;
-var t = 0;
-var timer = 0;
-requestAnimationFrame(plot);
-dt = 0.05;
-const resolution = 1000;
-const  numWP = 2;
+let t = 0;
+
+const dt = 0.05;
+const resolution = 2000;
+const numWP = 2;
 const lambda = 0.5;
-k = (2 * numWP * Math.PI) / lambda;
-dk = 0.05 * k;
-k1 = k + dk;
-k2 = k - dk;
-freq = 1;
-df = -0.5;
-f1 = freq + df;
-f2 = freq - df;
+
+const k = (2 * numWP * Math.PI) / lambda;
+const dk = 0.05 * k;
+const k1 = k + dk;
+const k2 = k - dk;
+const freq = 1;
+const df = -0.5;
+const f1 = freq + df;
+const f2 = freq - df;
+
+
 class Wave {
   constructor(A, k, freq, phase) {
     this.A = A;
@@ -34,48 +38,35 @@ class Wave {
   }
 }
 
-let wave1 = new Wave(1, k1, f1, 0);
-let wave2 = new Wave(1, k2, f2, 0);
+const amplitude = 1;
 
-
-const k_m = 0.2;
-const omega_m = 0;
+let wave1 = new Wave(amplitude, k1, f1, 0);
+let wave2 = new Wave(amplitude, k2, f2, 0);
+let envelope = new Wave(2 * amplitude, dk, df, 0);
 
 function plot() {
-
-  A = 1;
-  k = 2;
-  w = 1;
-  phi = 0;
-
-
-  max = 4 * pi;
-  for (i = 0; i < resolution + 1; i++) {
-    x = i * max / resolution;
-    w1 = wave1.getY(x, t);
-    w2 = wave2.getY(x, t);
+  const max = 4 * Math.PI;
+  for (let i = 0; i < resolution + 1; i++) {
+    const x = i * max / resolution;
+    const w1 = wave1.getY(x, t);
+    const w2 = wave2.getY(x, t);
     d1[i] = [x, w1];
-    d11 = [t*f1/k1, 1];
+    d11 = [t * f1 / k1, 1];
     d2[i] = [x, w2];
     d3[i] = [x, w1 + w2];
-    const a = -2 * 1 * Math.cos(df * t - dk * x );
-    const b = -1*a;
-    d5[i] = [x,a];
-    d4[i] = [x,b];
-
-    d7 = [t*df/dk +  (1.5*pi/dk) , 0];
-    // if((2 * 1 * Math.cos(df * t - dk * x ) )===0)
-    // {
-
-    // }
+    const upper = envelope.getY(x, t);
+    d5[i] = [x, upper];
+    d4[i] = [x, -1 * upper];
+    d7 = [t * df / dk + (1.5 * Math.PI / dk), 0];
   }
-//plot the data
-  var data1 = [{data: [d11], color: "red", shadowSize: 0, lines: {show: true},  points: {
+  var data1 = [{
+    data: [d11], color: "red", shadowSize: 0, lines: {show: true}, points: {
       radius: 3,
       show: true,
       symbol: "circle",
-      fill:true
-    }}, {data: d1, color: "green", shadowSize: 0, lines: {show: true}}];
+      fill: true
+    }
+  }, {data: d1, color: "green", shadowSize: 0, lines: {show: true}}];
   var data2 = [{data: d2, color: "blue", shadowSize: 0, lines: {show: true}}];
   var data4 = [{data: d3, color: "rgb(0, 255, 0)", shadowSize: 0, lines: {show: true}}, {
     data: d4,
@@ -89,17 +80,18 @@ function plot() {
       shadowSize: 0,
       lines: {show: true}
     },
-    {data: [d7], color: "red", shadowSize: 0, lines: {show: true},  points: {
+    {
+      data: [d7], color: "red", shadowSize: 0, lines: {show: true}, points: {
         radius: 5,
         show: true,
         symbol: "circle",
-        fill:true
-      }}];
-  $.plot(document.getElementById("placeholder1"), data1, {yaxis: {min: -1.5, max: 1.5}, xaxis: {min: 0, max:6}});
+        fill: true
+      }
+    }];
+  $.plot(document.getElementById("placeholder1"), data1, {yaxis: {min: -1.5, max: 1.5}, xaxis: {min: 0, max: 6}});
   $.plot(document.getElementById("placeholder2"), data2, {yaxis: {min: -1.5, max: 1.5}, xaxis: {min: 0, max: 6}});
   $.plot(document.getElementById("placeholder3"), data4, {yaxis: {min: -3.5, max: 3.5}, xaxis: {min: 0, max: 6}});
 
   requestAnimationFrame(plot);
   t = t + dt;
-  timer = timer + dt;
 }
